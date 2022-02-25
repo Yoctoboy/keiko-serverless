3f25f4d898d894f0633a40c52d4cca7a42553b43
Valentin Beggi <valentinbe@theodo.fr>
2022-02-25 17:46:20 +0100

    feat: simplify monorepo

---
+++ packages/frontend/src/pages/Home/Home.tsx
@@ -0,0 +1,19 @@
+import { Box } from '@mui/system';
+
+const Home = (): JSX.Element => {
+  return (
+    <Box
+      display="flex"
+      flexDirection="column"
+      justifyContent="center"
+      alignContent="center"
+      textAlign="center"
+      height="100vh"
+      maxWidth="100%"
+    >
+      toto
+    </Box>
+  );
+};
+
+export default Home;
